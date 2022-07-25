2ddb966e578ebeb509495f1e85a20f0bf27f5a8e
Luisda <lsanchezroldan@gmail.com>
2022-07-25 13:33:15 +0200

    Usamos un alias para pygame

--- the_quest/game.py
+++ the_quest/game.py
@@ -1,19 +1,19 @@
-import pygame
+import pygame as pg
 
 from the_quest import ALTO, ANCHO
 
 class the_quest:
     def __init__(self) -> None:
         print("Comienza el juego")
-        pygame.init()
-        self.display = pygame.display.set_mode((ANCHO, ALTO))
+        pg.init()
+        self.display = pg.display.set_mode((ANCHO, ALTO))
 
     def jugar(self):
         salir = False
         while not salir:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     salir = True
             self.display.fill((99, 99, 99))
-            pygame.display.flip()
+            pg.display.flip()
 
