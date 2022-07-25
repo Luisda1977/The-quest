fdfe3c2327a77dd62f8e891226a67eca47479713
Luisda <lsanchezroldan@gmail.com>
2022-07-25 11:25:21 +0200

    Creamos paquete the_quest (main, __init__, game)

---
+++ main.py
@@ -0,0 +1,7 @@
+from the_quest import ANCHO, ALTO
+from the_quest.game import the_quest
+
+if __name__ == "__main__":
+    print("Desde archivo main:")
+    print(f"El tamaño de la pantalla es {ANCHO}x{ALTO}")
+    the_quest()
---
+++ the_quest/game.py
@@ -0,0 +1,6 @@
+class the_quest:
+    def __init__(self) -> None:
+        print("Comienza el juego")
+
+if __name__ == "__main__":
+    the_quest()
