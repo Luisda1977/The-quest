7adcf14f881a80f00db10859bf7fbd228ecf9565
Luisda <lsanchezroldan@gmail.com>
2022-07-25 11:34:20 +0200

    Establecemos el modo de pantalla

--- the_quest/game.py
+++ the_quest/game.py
@@ -1,6 +1,12 @@
+import pygame
+
+from the_quest import ALTO, ANCHO
+
 class the_quest:
     def __init__(self) -> None:
         print("Comienza el juego")
+        pygame.init()
+        pygame.display.set_mode((ANCHO, ALTO))
 
 if __name__ == "__main__":
     the_quest()
